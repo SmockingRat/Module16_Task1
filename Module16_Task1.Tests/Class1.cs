1bf5990d5b734ace8126252a6cf96e5545fa8127
SmokingRat <halfbrikbarry@gmail.com>
2022-06-02 20:36:19 +0300

    Добавьте файлы проекта.

---
+++ Module16_Task1.Tests/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Module16_Task1;
+
+namespace Module16_Task1.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        [TestFixture]
+        public class Tests
+        {
+            [Test]
+            public void AdditionMustReturnValue()
+            {
+                var add = new Calculator();
+                var num = add.Additional(4, 5);
+                Assert.True(num == 9);
+            }
+
+            [Test]
+            public void SubstractionMustReturnValue()
+            {
+
+                var sub = new Calculator();
+                var num = sub.Subtraction(10, 4);
+                Assert.True(num == 6);
+
+            }
+
+            [Test]
+            public void MultiplicationMustReturnValue()
+            {
+
+                var mult = new Calculator();
+                var num = mult.Miltiplication(2, 3);
+                Assert.True(num == 6);
+
+            }
+
+            [Test]
+            public void DivisionMustReturnValue()
+            {
+
+                Assert.True(50 == 50);
+
+            }
+
+
+        }
+
+    }
+}
